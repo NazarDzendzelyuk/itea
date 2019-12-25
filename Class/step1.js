5d6d1df23b92840f6a054d4c7cec2eb813b5d218
Nazar <n.dzendzelyuk@gmail.com>
2019-12-25 19:23:42 +0200

    first commit

---
+++ Class/step1.js
@@ -0,0 +1,33 @@
+// методи стрічки
+
+// str.substring(0,1)
+// повертає стрічку з позиції 0 до позиції 1
+// не включаючи 1
+
+
+var str = "hello";
+console.log(str.substring(0,1));
+// виведе першу букву
+const str = 'qWeRtY';
+let i = 0;
+let result = '';
+
+//while(i < str.length) {
+//    if (str[i] === str[i].toUpperCase()) {
+//        result += str[i].toLowerCase();
+//    } else {
+//        result += str[i].toUpperCase();
+//    }
+//}
+
+// str.substr(0,1);
+// повертає стрічку з позиції 0 і довжиною 1
+
+
+// var str2 = "hello world";
+// console.log(str2.substr(1,3));
+// виведе ell
+
+
+
+//tasks/task2.js
